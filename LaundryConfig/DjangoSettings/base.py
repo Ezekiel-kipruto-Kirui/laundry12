4cb823a37c2f9f16225087a898f0c3d5d0365469
Ezekiel Kirui <youremail@example.com>
2025-10-18 16:58:34 +0300

    database changes

--- LaundryConfig/DjangoSettings/base.py
+++ LaundryConfig/DjangoSettings/base.py
@@ -159,18 +159,18 @@ WSGI_APPLICATION = 'LaundryConfig.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL'),
+#         conn_max_age=600,
+#     )
+# }
 
 
 # Password validation
