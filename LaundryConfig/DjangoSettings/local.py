44f770c762dd6860d7b76b0b49f6e158ab202d91
User <User@LAPTOP-US12U6NL.crownpaints.local>
2025-10-31 11:19:50 +0300

    final touches

--- LaundryConfig/DjangoSettings/local.py
+++ LaundryConfig/DjangoSettings/local.py
@@ -1,5 +1,14 @@
 from .base import *
 
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','*']
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:8080',
+    'http://localhost:8000',
+    'http://localhost:8080',
+]
+
+
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
