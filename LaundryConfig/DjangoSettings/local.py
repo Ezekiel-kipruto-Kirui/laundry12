db26dce9f9d8480f74514dcb8845861bc767c2ef
Ezekiel Kirui <youremail@example.com>
2025-11-03 11:06:40 +0300

    General changes

--- LaundryConfig/DjangoSettings/local.py
+++ LaundryConfig/DjangoSettings/local.py
@@ -1,16 +1,18 @@
 from .base import *
+if DEBUG:
+    ALLOWED_HOSTS = ['127.0.0.1', 'localhost','*']
+    CSRF_TRUSTED_ORIGINS = [
+        'http://127.0.0.1:8000',
+        'http://127.0.0.1:8080',
+        'http://localhost:8000',
+        'http://localhost:8080',
+    ]
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','*']
-CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:8080',
-    'http://localhost:8000',
-    'http://localhost:8080',
-]
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+
