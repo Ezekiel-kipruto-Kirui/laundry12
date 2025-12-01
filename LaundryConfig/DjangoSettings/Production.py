6f8bec4e157ad9c42dfe5a1a43a186f25bb01782
Ezekiel Kipruto Kirui <155768392+Ezekiel-kipruto-Kirui@users.noreply.github.com>
2025-12-01 16:05:45 +0300

    Update Production.py

--- LaundryConfig/DjangoSettings/Production.py
+++ LaundryConfig/DjangoSettings/Production.py
@@ -4,28 +4,28 @@ import os
 
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-if not DEBUG:
-    ALLOWED_HOSTS = ['www.cleanpage.shop', 'cleanpage.shop', 'clean-page-laundry.onrender.com']
-    CSRF_TRUSTED_ORIGINS = [
-        'https://www.cleanpage.shop',
-        'https://cleanpage.shop',
-        'https://clean-page-laundry.onrender.com'
-    ]
 
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.getenv('DATABASE_URL'),
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
+ALLOWED_HOSTS = ['www.cleanpage.shop', 'cleanpage.shop', 'clean-page-laundry.onrender.com']
+CSRF_TRUSTED_ORIGINS = [
+    'https://www.cleanpage.shop',
+    'https://cleanpage.shop',
+    'https://clean-page-laundry.onrender.com'
+]
 
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_BROWSER_XSS_FILTER = True
-    X_FRAME_OPTIONS = "DENY"
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = "DENY"
